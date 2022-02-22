2626d53e29e1547aadb6206c03e1e9051eea3b02
nitesh585 <niteshyadav585@gmail.com>
2022-02-22 18:07:57 +0530

    feat: add controller

---
+++ controllers/user/user.go
@@ -0,0 +1,19 @@
+package user
+
+import "github.com/gin-gonic/gin"
+
+func Signup(gin *gin.Context) {
+
+}
+
+func Login(gin *gin.Context) {
+
+}
+
+func GetUsers(gin *gin.Context) {
+
+}
+
+func GetUser(gin *gin.Context) {
+
+}
