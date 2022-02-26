038dbefda85d96ee7248a0a0c1a74521d7dfa57e
nitesh585 <niteshyadav585@gmail.com>
2022-02-26 22:41:57 +0530

    add user controller and middleware

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -1,9 +1,78 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"rail/database"
+	models "rail/models/user"
+	"time"
 
-func Signup(gin *gin.Context) {
+	"context"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.MongoClient, "user")
+var validate *validator.Validate
+
+func HashPassword() {
+
+}
+
+func VerifyPassword() {
+
+}
+
+func Signup() gin.HandlerFunc {
+	return func(g *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+		var user models.User
+
+		if err := g.BindJSON(&user); err != nil {
+			g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validate = validator.New()
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			g.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			g.JSON(http.StatusBadRequest, gin.H{"error": "Error! occured while checking for email"})
+			return
+		}
+
+		if count > 0 {
+			// log.Panic("Email is already exists.")
+			g.JSON(http.StatusBadRequest, gin.H{"error": "User already exists."})
+			return
+		}
+
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Id = primitive.NewObjectID()
+		user.User_id = user.Id.Hex()
+		user.Token = "hello"
+
+		insertedNumber, insertErr := userCollection.InsertOne(ctx, user)
+		defer cancel()
+		if insertErr != nil {
+			g.JSON(http.StatusBadGateway, gin.H{"error": insertErr.Error()})
+			return
+		}
+
+		g.JSON(http.StatusOK, gin.H{"insertedNumber": insertedNumber})
+	}
 }
 
 func Login(gin *gin.Context) {
---
+++ middleware/authMiddleWare.go
@@ -0,0 +1,7 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func Authenticate(gin *gin.Context) {
+
+}
