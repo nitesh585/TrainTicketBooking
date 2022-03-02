09ad6fefa80317a2a2ce1b91a255681410141756
nitesh585 <niteshyadav585@gmail.com>
2022-03-02 23:50:41 +0530

    add train models

--- models/train/train.go
+++ models/train/train.go
@@ -1,1 +1,29 @@
 package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TrainDetails struct {
+	Id          primitive.ObjectID `bson:"_id"`
+	TrainNumber int16              `json:"trainNumber"`
+	ArrivalTime time.Time          `json:"arrivalTime"`
+	Departure   time.Time          `json:"departureTime"`
+}
+
+type Station struct {
+	Id           primitive.ObjectID `json:"_id"`
+	Name         string             `json:"name"`
+	TrainDetails TrainDetails       `json:"trainDetails"`
+}
+
+type Train struct {
+	Id          primitive.ObjectID `json:"_id"`
+	Name        string             `json:"name"`
+	TrainNumber string             `json:"trainNumber"`
+	Source      string             `json:"source"`
+	Destination string             `json:"destination"`
+	Stations    []Station          `json:"stations"`
+}
