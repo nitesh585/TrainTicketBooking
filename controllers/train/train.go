81f47e555672b1bebf78bd5ba6a93c4b567fa9a5
nitesh585 <niteshyadav585@gmail.com>
2022-03-07 16:48:59 +0530

    add train/serachroute with model

--- controllers/train/train.go
+++ controllers/train/train.go
@@ -1,1 +1,44 @@
-package train
+package controller
+
+import (
+	"context"
+	"net/http"
+	"rail/database"
+	models "rail/models/train"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SearchQuery struct {
+	Source      string
+	Destination string
+	Date        string
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.MongoClient, "train")
+
+func SearchRoute() gin.HandlerFunc {
+	return func(g *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		defer cancel()
+
+		var search SearchQuery
+
+		if err := g.BindJSON(&search); err != nil {
+			g.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			return
+		}
+
+		var trainDetails models.Train
+
+		err := userCollection.FindOne(ctx, bson.D{{"tags", bson.D{{"$all", bson.A{"red", "blank"}}}}}).Decode(&trainDetails)
+		if err != nil {
+			g.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			return
+		}
+
+	}
+}
--- models/train/train.go
+++ models/train/train.go
@@ -1,29 +1,26 @@
 package model
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type TrainDetails struct {
-	Id          primitive.ObjectID `bson:"_id"`
-	TrainNumber int16              `json:"trainNumber"`
-	ArrivalTime time.Time          `json:"arrivalTime"`
-	Departure   time.Time          `json:"departureTime"`
-}
-
-type Station struct {
-	Id           primitive.ObjectID `json:"_id"`
-	Name         string             `json:"name"`
-	TrainDetails TrainDetails       `json:"trainDetails"`
-}
-
 type Train struct {
-	Id          primitive.ObjectID `json:"_id"`
-	Name        string             `json:"name"`
-	TrainNumber string             `json:"trainNumber"`
-	Source      string             `json:"source"`
-	Destination string             `json:"destination"`
-	Stations    []Station          `json:"stations"`
+	Id              primitive.ObjectID `json:"_id"`
+	TrainName       string             `json:"trainname"`
+	TrainNumber     string             `json:"trainNumber"`
+	FromStationCode string             `json:"source"`
+	ToStationCode   string             `json:"destination"`
+	ArrivalTime     string             `json:"avlTime"`
+	DepartureTime   string             `json:"depTime"`
+	Distance        string             `json:"distance"`
+	Duration        string             `json:"duration"`
+	RunningMon      string             `json:"runningMon"`
+	RunningTue      string             `json:"runningTue"`
+	RunningWed      string             `json:"runningWed"`
+	RunningThr      string             `json:"runningThr"`
+	RunningFri      string             `json:"runningFri"`
+	RunningSat      string             `json:"runningSat"`
+	RunningSun      string             `json:"runningSun"`
+	AvlClasses      []string           `json:"avlClasses"`
+	Stations        []string           `json:"stations"`
 }
