df3ebd92e692e928fddd3804dfab54bec8349bbd
nitesh585 <niteshyadav585@gmail.com>
2022-03-07 17:04:21 +0530

    add helper method of serach route

---
+++ helpers/train/trainSearch.go
@@ -0,0 +1,38 @@
+package helper
+
+import (
+	models "rail/models/train"
+)
+
+var weekDayToKey = make(map[string]string)
+
+func isTrainAvailableOnGivenWeekDay(weekday string, trainRecord models.Train) bool {
+	switch weekday {
+	case "Tuesday":
+		return trainRecord.RunningTue == "Y"
+	case "Wednesday":
+		return trainRecord.RunningWed == "Y"
+	case "Thursday":
+		return trainRecord.RunningThr == "Y"
+	case "Friday":
+		return trainRecord.RunningFri == "Y"
+	case "Saturday":
+		return trainRecord.RunningSat == "Y"
+	case "Sunday":
+		return trainRecord.RunningSun == "Y"
+
+	default:
+		return trainRecord.RunningMon == "Y"
+	}
+}
+
+func FilterDetailsOnWeekdayAwailability(weekday string, trainDetails []models.Train) []models.Train {
+	var filteredTrainDetails []models.Train
+	for _, rec := range trainDetails {
+		if isTrainAvailableOnGivenWeekDay(weekday, rec) {
+			filteredTrainDetails = append(filteredTrainDetails, rec)
+		}
+	}
+
+	return filteredTrainDetails
+}
