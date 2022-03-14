1ef06a97e39ed017d88fa49da59fb0b5c825dee1
nitesh585 <niteshyadav585@gmail.com>
2022-03-14 13:26:10 +0530

    generate payment link using user and train details

--- controllers/payment/payment.go
+++ controllers/payment/payment.go
@@ -3,6 +3,10 @@ package controller
 import (
 	"net/http"
 	"os"
+	paymentHelper "rail/helpers/payment"
+	authHelper "rail/helpers/user"
+
+	models "rail/models/payment"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -21,19 +25,43 @@ func PayForTicket() gin.HandlerFunc {
 		key_id := os.Getenv("RAZORPAY_KEY_ID")
 		key_secret := os.Getenv("RAZORPAY_KEY_SECRET")
 
+		var payTicketDetails models.PayTicket
+		if err := c.BindJSON(&payTicketDetails); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		clientToken := c.Request.Header.Get("token")
+		claims, _ := authHelper.VerifyToken(clientToken)
+
+		referenceID, err := paymentHelper.RandRefrenceID(8)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
 		client := razorpay.NewClient(key_id, key_secret)
 		data := gin.H{
-			"amount":   50000,
-			"currency": "INR",
-			"receipt":  "some_receipt_id",
+			"amount":       payTicketDetails.Amount,
+			"currency":     payTicketDetails.Currency,
+			"reference_id": referenceID,
+			"customer": models.PayCustomerDetails{
+				Name:  claims.FirstName + " " + claims.LastName,
+				Email: claims.Email,
+			},
+			"notes": models.PayNotes{
+				UserID:        claims.User_id,
+				DateOfBooking: payTicketDetails.Date,
+				ClassBooked:   payTicketDetails.ClassBooking,
+				TrainId:       payTicketDetails.TrainID,
+			},
 		}
-		body, err := client.Order.Create(data, nil)
+		body, err := client.PaymentLink.Create(data, nil)
 
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err, "body": body})
 			return
 		}
-
 		c.JSON(http.StatusOK, gin.H{"link": body})
 	}
 }
